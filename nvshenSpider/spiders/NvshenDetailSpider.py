795ab2fb1e3a7b8434c8a13e39621f0a31fa2302
wuxubaiyang <wuxubaiyang@gmail.com>
2018-08-13 13:22:50 +0800

    删除jobinfo文件 修复详情页爬取的问题

--- nvshenSpider/spiders/NvshenDetailSpider.py
+++ nvshenSpider/spiders/NvshenDetailSpider.py
@@ -17,8 +17,11 @@ class NvshendetailspiderSpider(scrapy.Spider):
     allowed_domains = ['nvshens.com']
 
     def start_requests(self):
-        for item in collection.find({"album_photos": {"$exists": False}}):
-            yield scrapy.Request(item["detail_url"], dont_filter=True)
+        # for item in collection.find({"album_photos": {"$exists": False}}):
+        #     yield scrapy.Request(item["detail_url"], dont_filter=True)
+        for item in collection.find():
+            if item["album_count"] != item["album_photos"]:
+                print(item["detail_url"])
 
     def parse(self, response):
         soup = Soup(response.body, "lxml")
@@ -27,13 +30,22 @@ class NvshendetailspiderSpider(scrapy.Spider):
         if web_path.endswith(".html"):  # 详情页分页图片解析
             item["detail_url"] = response.url[:response.url.rindex("/") + 1]
             photos = []
-            for i in soup.find("div", class_="gallery_wrapper").find_all("img"):
-                photos.append(i.get("src"))
+            pgallery = soup.find("ul", id="pgallery")
+            hgallery = soup.find("ul", id="hgallery")
+            if pgallery is not None:
+                [photos.append(i.get("src")) for i in pgallery.find_all("img")]
+            elif hgallery is not None:
+                [photos.append(i.get("src")) for i in hgallery.find_all("img")]
             item["album_photos"] = photos
         else:  # 详情页信息解析
             item["detail_url"] = response.url
             tags = []
-            for a in soup.find("div", class_="album_tags").find_all("a"):
+            album_tags = soup.find("div", class_="album_tags")
+            if album_tags is None:
+                collection.remove({"detail_url": response.url})
+                print("数据不存在，已删除")
+                return
+            for a in album_tags.find_all("a"):
                 href = a.get("href")
                 tags.append({
                     "name": a.text,
@@ -44,14 +56,23 @@ class NvshendetailspiderSpider(scrapy.Spider):
             info = soup.find("div", id="dinfo", class_="albumInfo").text
             item["create_date"] = re.search('\d{4}/\d{1,2}/\d{1,2}', info).group().strip()
             item["album_count"] = int(re.search('\d{1,3}', info).group().strip())
-            item["views"] = re.search(' \d{1,6} ', info).group().strip()
+            try:
+                item["views"] = re.search(' \d{1,8} ', info).group().strip()
+            except:
+                item["views"] = "0"
             photos = []
-            for i in soup.find("div", class_="gallery_wrapper").find_all("img"):
-                photos.append(i.get("src"))
+            pgallery = soup.find("ul", id="pgallery")
+            hgallery = soup.find("ul", id="hgallery")
+            if pgallery is not None:
+                [photos.append(i.get("src")) for i in pgallery.find_all("img")]
+            elif hgallery is not None:
+                [photos.append(i.get("src")) for i in hgallery.find_all("img")]
             item["album_photos"] = photos
         yield item  # 数据存储
         # 获取详情页的图片分页
-        pages_tag = soup.find("div", id="pages").find_all("a", class_="a1")
-        next_page_url = urljoin(self.base_url, pages_tag[len(pages_tag) - 1].get("href"))
-        if next_page_url.endswith(".html"):
-            yield scrapy.Request(next_page_url)  # 请求分页
+        pages = soup.find("div", id="pages")
+        if pages is not None:
+            pages_tag = pages.find_all("a", class_="a1")
+            next_page_url = urljoin(self.base_url, pages_tag[len(pages_tag) - 1].get("href"))
+            if next_page_url.endswith(".html"):
+                yield scrapy.Request(next_page_url)  # 请求分页
